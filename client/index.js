e9035a678b08cd38b875b7d2181a056c28707c5c
LeoMinistry <lylsnrc@126.com>
2023-07-19 15:29:43 +0800

    COMPLETE GAME YEAH!!

--- client/index.js
+++ client/index.js
@@ -1,16 +1,27 @@
 // Initialize WebSocket connection and event handlers
-var ws, id;
+var ws, id, refresh, pace = 500;
 function setup(obj) {
     log("开始连接")
     ws = new WebSocket("ws://localhost:1145");
     // Listen for the connection open event then call the sendMessage function
     ws.onopen = function (e) {
-        sendMessage("WAITING FIRST username:" + document.getElementById("username").value)
+        sendMessage("INITALIZE username:" + document.getElementById("username").value);
+        refresh = setInterval(() => {
+            if (!isNaN(new Date() - delay - pace)) {
+                document.getElementById("delay").innerText = Math.abs(new Date() - delay - pace);
+                sendMessage("GAME delay id=" + id + ",delay=" + Math.abs(new Date() - delay - pace));
+            } else {
+                sendMessage("GAME delay id=" + id + ",delay=-");
+            }
+            delay = new Date();
+            sendMessage("GAME require-data");
+        }, pace);
     }
     // Listen for the close connection event
     ws.onclose = function (e) {
         log("Disconnected:" + e.reason);
         obj.disabled = "";
+        document.getElementById("game").innerText = "失去服务器连接";
         document.getElementById("error").innerText = "连接失败，请重试";
     }
     // Listen for connection errors
@@ -20,17 +31,16 @@ function setup(obj) {
     }
     // Listen for new messages arriving at the client
     ws.onmessage = function (e) {
-        if (e.data.search(/^WAITING id:/) != -1) {
+        if (e.data.search(/^WAIT id:/) != -1) {
             id = e.data.split(":")[1];
             log("ID:" + id);
             enter(id);
-        } else if (e.data.search(/^GAMING data:/) != -1) {
-            listplayers(e.data.replace("GAMING data:", ""));
-            sendMessage("GAMING delay id=" + id + ",delay=" + caldelay());
+        } else if (e.data.search(/^GAME data:/) != -1) {
+            display(JSON.parse(e.data.replace("GAME data:", "")));
         }
-        log(e.data);
     }
     document.body.onunload = () => {
+        clearInterval(refresh);
         log("断开连接");
         ws.close();
     }
@@ -38,45 +48,118 @@ function setup(obj) {
 // Send a message on the WebSocket.
 function sendMessage(msg) {
     ws.send(msg);
-    log("发送信息：" + msg);
+    // log("发送信息：" + msg);
 }
 function log(msg) {
-    document.getElementById("console").innerText += (msg + "\n")
+    // document.getElementById("console").innerText += (msg + "\n")
 }
-function caldelay() {
-    if (!isNaN(new Date() - delay - 1000)) {
-        let temp = new Date() - delay - 1000
-        document.getElementById("delay").innerText = temp;
-        delay = new Date();
-        return temp;
-    } else {
-        delay = new Date();
-        return 0;
-    }
-}
-
 function enter(id) {
     document.getElementById("preparation").style.display = "none";
-    document.getElementById("room-list").style.display = "unset";
+    document.getElementById("game").style.display = "unset";
     document.getElementById("id-display").innerText = id;
     document.getElementById("username-display").innerText = document.getElementById("username").value;
 }
-
-function listplayers(ouser) {
-    let user = JSON.parse(ouser);
-    document.getElementById("user-list").innerHTML = "";
-    for (const userown in user["user"]) {
-        li = document.createElement("li");
-        log(userown + " " + id + " " + JSON.stringify(user["user"]));
-        li.innerText = "#" + (parseInt(userown) + 1) + "    " + user["user"][userown]["name"] + " ";
-        if (user["user"][userown]["op"]) {
-            li.innerText += "(房主) "
-        }
-        if (userown != id) {
-            li.innerText += user["user"][userown]["delay"] + "ms";
-        } else {
-            li.innerText += "(我)";
-        }
-        document.getElementById("user-list").appendChild(li);
+function display(data) {
+    if (data["end"]) {
+        //游戏结束
+        document.getElementById("game").style.display = "none";
+        document.getElementById("finish").style.display = "unset";
+        document.getElementById("players-count").innerText = Object.keys(data["user"]).length;
+        document.getElementById("round-count").innerText = data["data"]["round"];
+        document.getElementById("words-count").innerText = data["data"]["word"].length;
+        clearInterval(refresh);
+        ws.close();
+        return;
     }
+    //读取消息队列
+    const list = document.getElementById("message-display");
+    list.innerHTML = "";
+    for (const mess of data["message"]) {
+        tempstr = "<li>";
+        if (mess["type"] == "system") {
+            tempstr += "【系统】";
+            if (mess["content"] == "kicked") {
+                if (mess["id"] == id) {
+                    document.getElementById("game").innerHTML = "<b>非常抱歉，您已被房主踢出</b>";
+                    clearInterval(refresh);
+                    return;
+                }
+                else {
+                    tempstr += "ID为" + mess["id"] + "的玩家" + mess["name"] + "被踢出游戏";
+                }
+            } else if (mess["content"] == "closed") {
+                tempstr += "ID为" + mess["id"] + "的玩家" + mess["name"] + "失去了连接";
+            } else if (mess["content"] == "joined") {
+                tempstr += "ID为" + mess["id"] + "的玩家" + mess["name"] + "加入了游戏";
+            }
+        } else if (mess["type"] == "game") {
+            tempstr += "【游戏】";
+            if (mess["content"] == "opened-letter") {
+                tempstr += "ID为" + mess["id"] + "的玩家" + mess["name"] + "翻开了字母" + mess["letter"];
+            } else if (mess["content"] == "guess") {
+                tempstr += "ID为" + mess["id"] + "的玩家" + mess["name"] + "猜" + (mess["right"] ? "对" : "错") + "了第" + mess["order"] + "个单词";
+            }
+        }
+        tempstr += "</li>";
+        list.innerHTML += tempstr;
+    }
+    list.scrollTop = list.scrollHeight;
+    const operator = document.getElementById("user-list");
+    tempstr = "";
+    for (const userown in data["user"]) {
+        if (Object.hasOwnProperty.call(data["user"], userown)) {
+            tempstr += "<li>#" + userown + "&Tab;" + data["user"][userown]["name"] + "&Tab;";
+            if (data["user"][userown]["operator"]) {
+                tempstr += "(房主) ";
+            } if (userown != id) {
+                tempstr += data["user"][userown]["delay"] + "ms";
+                if (data["user"][id]["operator"]) {
+                    tempstr += "<button onclick='kick(this)' title=" + userown + ">踢出</button>";
+                }
+            } else {
+                tempstr += " <b>(我)</b>";
+            }
+            tempstr += "</li>"
+        }
+    }
+    operator.innerHTML = tempstr;
+    const wordlist = document.getElementById("word-list");
+    let wordtempstr = "";
+    for (const word of data["data"]["words"]) {
+        wordtempstr += "<li>" + word + "</li>";
+    }
+    wordlist.innerHTML = wordtempstr;
+    document.getElementById("opened-letter").innerText = data["data"]["openedletter"].length > 0 ? data["data"]["openedletter"].join("、") : "(无)";
+    document.getElementById("guess-order").max = data["data"]["words"].length;
+    document.getElementById("round").innerText = data["data"]["round"];
+    document.getElementById("left-guess").innerText = data["data"]["leftguess"];
+}
+
+function kick(obj) {
+    sendMessage("GAME kick id=" + obj.title);
+}
+
+function changeMethod() {
+    if (document.getElementById("method").value == "open-letter") {
+        document.getElementById("open-letter-border").style.display = "unset";
+        document.getElementById("guess-border").style.display = "none";
+    } else {
+        document.getElementById("open-letter-border").style.display = "none";
+        document.getElementById("guess-border").style.display = "unset";
+    }
+}
+
+function send(obj) {
+    if (document.getElementById("method").value == "open-letter") {
+        sendMessage("GAME open-letter id=" + id + ",open-letter=" + document.getElementById("open-letter").value);
+        document.getElementById("open-letter").value = "";
+    } else {
+        sendMessage("GAME guess id=" + id + ",order=" + document.getElementById("guess-order").value + ",guess-word=" + document.getElementById("guess-word").value);
+        document.getElementById("guess-order").value = 1;
+        document.getElementById("guess-word").value = "";
+    }
+    obj.disabled = "disabled";
+    setTimeout(() => {
+        obj.disabled = "";
+    }, 1000);
 }
--- server/main.js
+++ server/main.js
@@ -1,54 +1,133 @@
 const ws = require("nodejs-websocket");
-var identify = 0;
+var identify = 1;
 var storage = {
     "user": {}, "data": {
-        "words": [
-        ]
-    }
+        "words": [],
+        "openedletter": [],
+        "round": 1,
+        "leftguess": 0
+    }, "message": [], "end": false
 }
-var wordsResource;
-
+var wordAnswer = []
 var _server = ws.createServer(conn => {
-    var identifier, refresh;
+    let id;
     // 接收客户端返回的数据
     conn.on("text", function (str) {
-        if (str.search(/^WAITING/) != -1) {
-            if (str.search(/^WAITING FIRST/) != -1) {
-                identifier = identify;
-                identify++;//鉴别身份
-                //创建用户,op=管理员
-                storage["user"][identifier] = { "name": str.split(":")[1], "delay": "-", "op": identifier == 0 ? true : false };
-                conn.send("WAITING id:" + identifier);//发送ID
-                conn.send("GAMING data:" + JSON.stringify(storage));//发送初始化游戏数据
-                refresh = setInterval(() => {
-                    conn.send("GAMING data:" + JSON.stringify(storage));
-                }, 1000);//定时发送游戏数据
-            }
+        if (str.search(/^INITALIZE/) != -1) {
+            id = identify;
+            identify++;//鉴别身份
+            //创建用户,op=管理员
+            console.warn(Object.keys(storage["user"]).length);
+            storage["user"][id] = { "name": str.split(":")[1], "delay": "-", "operator": Object.keys(storage["user"]).length == 0 ? true : false };
+            storage["message"].push({
+                "type": "system",
+                "content": "joined",
+                "id": id,
+                "name": str.split(":")[1]
+            })
+            conn.send("WAIT id:" + id);//发送ID
         } else {
-            if (str.search(/^GAMING delay id=/) != -1) {
-                storage["user"][identifier]["delay"] = str.split("delay=")[1];//更改延时
+            if (str.search(/^GAME/) != -1) {
+                if (str.search(/^GAME delay id=/) != -1) {
+                    if (id in storage["user"]) {
+                        storage["user"][id]["delay"] = str.split("delay=")[1];//更改延时
+                    }
+                } else if (str.search(/^GAME kick id=/) != -1) {
+                    //踢出用户
+                    storage["message"].push({
+                        "type": "system",
+                        "content": "kicked",
+                        "id": str.split("id=")[1],
+                        "name": storage["user"][str.split("id=")[1]]["name"]
+                    });
+                } else if (str == "GAME require-data") {
+                    conn.send("GAME data:" + JSON.stringify(storage));
+                    console.log(JSON.stringify(storage));
+                }
+                else if (str.search(/^GAME open-letter/) != -1) {
+                    let temp = storage["data"]["words"];
+                    storage["data"]["words"] = [];
+                    for (var i = 0; i < wordAnswer.length; i++) {
+                        var newword = "";
+                        for (var j = 0; j < wordAnswer[i].length; j++) {
+                            if (temp[i][j] == "*") {
+                                if (str.split("open-letter=")[1].toLowerCase() == wordAnswer[i][j].toLowerCase()) {
+                                    newword += wordAnswer[i][j];
+                                } else {
+                                    newword += "*";
+                                }
+                            } else {
+                                newword += wordAnswer[i][j];
+                            }
+                        }
+                        storage["data"]["words"].push(newword);
+                    }
+                    storage["data"]["round"]++;
+                    if (storage["data"]["openedletter"].indexOf(str.split("open-letter=")[1].toLowerCase()) == -1) {
+                        storage["data"]["openedletter"].push(str.split("open-letter=")[1].toLowerCase())
+                    }
+                    storage["message"].push({
+                        "type": "game",
+                        "content": "opened-letter",
+                        "id": str.split("id=")[1].split(",")[0],
+                        "letter": str.split("open-letter=")[1],
+                        "name": storage["user"][str.split("id=")[1].split(",")[0]]["name"]
+                    });
+                } else if (str.search(/^GAME guess/) != -1) {
+                    if (wordAnswer[str.split("order=")[1].split(",guess-word=")[0] - 1] == str.split(",guess-word=")[1]) {
+                        storage["message"].push({
+                            "type": "game",
+                            "content": "guess",
+                            "right": true,
+                            "id": str.split("id=")[1].split(",order=")[0],
+                            "order": str.split("order=")[1].split(",guess-word=")[0],
+                            "name": storage["user"][str.split("id=")[1].split(",order=")[0]]["name"]
+                        });
+                        storage["data"]["words"][str.split("order=")[1].split(",guess-word=")[0] - 1] = wordAnswer[str.split("order=")[1].split(",guess-word=")[0] - 1];
+                    } else {
+                        storage["message"].push({
+                            "type": "game",
+                            "content": "guess",
+                            "right": false,
+                            "id": str.split("id=")[1].split(",order=")[0],
+                            "order": str.split("order=")[1].split(",guess-word=")[0],
+                            "name": storage["user"][str.split("id=")[1].split(",order=")[0]]["name"]
+                        });
+                    }
+                    storage["data"]["round"]++;
+                }
+                //计算剩余待猜单词数量
+                storage["data"]["leftguess"] = storage["data"]["words"].length;
+                for (var k = 0; k < storage["data"]["words"].length; k++) {
+                    if (storage["data"]["words"][k].search(/\*/) == -1) {
+                        storage["data"]["leftguess"]--;
+                    }
+                }
+                if (storage["data"]["leftguess"] == 0) {
+                    storage["end"] = true;
+                }
             }
         }
     });
 
     //客户端关闭连接
     conn.on("close", function (e) {
-        console.log("断开连接 ID", identifier);
-        clearInterval(refresh);
-        delete storage["user"][identifier];
+        console.log("断开连接 ID", id);
+        storage["message"].push({
+            "type": "system",
+            "content": "closed",
+            "id": id,
+            "name": storage["user"][id]["name"]
+        });
+        remove(id);
     });
 
     conn.on("error", function (err) {
         //error
         console.log(err, "连接报错");
-        conn.close();
     });
 });
-const port = 1145;
-loadDoc();
-_server.listen(port, function () {
-    console.log("监听ws://localhost:" + port);
-})
+
 
 function loadDoc() {
     // 引入Node.js文件系统模块：
@@ -59,6 +138,51 @@ function loadDoc() {
         //当文件读取失败时，可以获取到err的值，输出错误信息
         if (err) { throw err }
         //当文件读取成功时，可以获取到data的值，输出响应的内容
-        wordsResource = data.toString().split("\r\n");
+        // let temparray = ;
+        wordAnswer = getRandomArrayElements(data.toString().split("\r\n"), 5)
+        for (const each of wordAnswer) {
+            var newword = "";
+            for (let i of each) {
+                if (i.search(/[A-Za-z0-9]/) != -1) {
+                    newword += "*"
+                } else {
+                    newword += i;
+                }
+            }
+            storage["data"]["words"].push(newword);
+        }
     })
 }
+
+function remove(identify) {
+    if (identify in storage["user"]) {
+        console.warn(identify, storage["user"]);
+        if (storage["user"][identify]["operator"] && Object.keys(storage["user"]).length > 1) {//房主继承
+            let index = identify + 1;
+            while (!(index in storage["user"])) {
+                index++;
+            }
+            storage["user"][index]["operator"] = true;
+        }
+        delete storage["user"][identify];
+    }
+}
+
+function getRandomArrayElements(arr, count) {
+    var shuffled = arr.slice(0), i = arr.length, min = i - count, temp, index;  //只是声明变量的方式, 也可以分开写
+    while (i-- > min) {
+        //console.log(i);
+        index = Math.floor((i + 1) * Math.random()); //这里的+1 是因为上面i--的操作  所以要加回来
+        temp = shuffled[index];  //即值交换
+        shuffled[index] = shuffled[i];
+        shuffled[i] = temp;
+        //console.log(shuffled);
+    }
+    return shuffled.slice(min);
+}
+
+const port = 1145;
+loadDoc();
+_server.listen(port, function () {
+    console.log("监听ws://localhost:" + port);
+})
